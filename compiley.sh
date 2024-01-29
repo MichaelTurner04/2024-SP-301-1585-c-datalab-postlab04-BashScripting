802f3e8703b87bbf9380000fbfe37723f49ed8dd
stcscpp <stcscpp@umsystem.edu>
2024-01-28 23:00:40 -0600

    init

---
+++ compiley.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+compile_file()
+{
+    INPUT="$1"
+    OUTPUT="$2"
+
+    echo ""
+    echo "Function parameters"
+    echo "- compile_file's first parameter (\$1) is \"$1\""
+    echo "- compile_file's second parameter (\$2) is \"$2\""
+    echo "- \$INPUT is \"$INPUT\""
+    echo "- \$OUTPUT is \"$OUTPUT\""
+    echo ""
+
+    g++ -o "$OUTPUT" "$INPUT"
+}
+
+echo "Positional parameters:"
+echo "- First positional parameter (\$1) is \"$1\""
+echo "- Second positional parameter (\$2) is \"$2\""
+echo "- Joined positional parameters (\$@) is \"$@\""
+
+compile_file $@
---
+++ program.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout << "It's-a me, GCC!" << endl;
+  return 0;
+}
