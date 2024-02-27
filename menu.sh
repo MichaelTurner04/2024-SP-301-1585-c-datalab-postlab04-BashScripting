24a1c45dff891ff0bdf906597daa623f7b13c18e
mturner <mjt6yf@umsystem.edu>
2024-02-27 15:40:22 -0600

    commit 1

---
+++ menu.sh
@@ -0,0 +1,54 @@
+
+for file in *
+do
+echo "v) View" $file
+echo "e) Edit " $file
+echo "c) Compile " $file
+echo "x) Execute " $file
+echo "q) Quit" $file
+input=""
+read input
+case $input in
+	v)
+		echo "Viewing file."
+		doThatThing $file "v"
+		;;
+	e)
+		echo "Editing file."
+		doThatThing $file "e"
+		;;
+	c)
+		echo "Compiling file."
+		doThatThing $file "c"
+		;;
+	x)
+		echo "executing file."
+		doThatThing $file "x"
+		;;
+	q)
+		echo "quitting"
+		exit
+		;;
+	*)
+		echo "INVALID RESPONSE"
+		echo "skipping this file!"
+		;;
+esac
+done
+doThatThing(file, action)
+{
+	case $action in
+		v)
+			less $file
+			;;
+		e)
+			nano $file
+			;;
+		c)
+			g++ $file
+			;;
+		x)
+			./$file
+			;;
+	esac
+}
