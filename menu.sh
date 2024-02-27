b6350980c5909af53484ffcd9ab1d8d60ad5993e
Michael Turner <mjt6yf@umsystem.edu>
2024-02-27 15:56:29 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,38 +1,38 @@
 for file in *
-do
-echo "v) View" $file
-echo "e) Edit " $file
-echo "c) Compile " $file
-echo "x) Execute " $file
-echo "q) Quit" $file
-input=""
-read input
-case $input in
-	v)
-		echo "Viewing file."
-		doThatThing $file "v"
-		;;
-	e)
-		echo "Editing file."
-		doThatThing $file "e"
-		;;
-	c)
-		echo "Compiling file."
-		doThatThing $file "c"
-		;;
-	x)
-		echo "executing file."
-		doThatThing "$file" "x"
-		;;
-	q)
-		echo "quitting"
-		exit
-		;;
-	*)
-		echo "INVALID RESPONSE"
-		echo "skipping this file!"
-		;;
-esac
+	do
+	echo "v) View" $file
+	echo "e) Edit " $file
+	echo "c) Compile " $file
+	echo "x) Execute " $file
+	echo "q) Quit" $file
+	input=""
+	read input
+	case $input in
+		v)
+			echo "Viewing file."
+			doThatThing $file "v"
+			;;
+		e)
+			echo "Editing file."
+			doThatThing $file "e"
+			;;
+		c)
+			echo "Compiling file."
+			doThatThing $file "c"
+			;;
+		x)
+			echo "executing file."
+			doThatThing "$file" "x"
+			;;
+		q)
+			echo "quitting"
+			exit
+			;;
+		*)
+			echo "INVALID RESPONSE"
+			echo "skipping this file!"
+			;;
+	esac
 done
 doThatThing()
 {
