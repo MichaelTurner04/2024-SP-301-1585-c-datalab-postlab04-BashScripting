d991af69e7a4faecedb229a6ae6c145df603f435
Michael Turner <mjt6yf@umsystem.edu>
2024-02-27 16:02:07 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,55 +1,55 @@
 for file in *
 	do
-	echo "v) View" $file
-	echo "e) Edit " $file
-	echo "c) Compile " $file
-	echo "x) Execute " $file
-	echo "q) Quit" $file
-	input=""
-	read input
-	case $input in
-		v)
-			echo "Viewing file."
-			doThatThing $file "v"
-			;;
-		e)
-			echo "Editing file."
-			doThatThing $file "e"
-			;;
-		c)
-			echo "Compiling file."
-			doThatThing $file "c"
-			;;
-		x)
-			echo "executing file."
-			doThatThing "$file" "x"
-			;;
-		q)
-			echo "quitting"
-			exit
-			;;
-		*)
-			echo "INVALID RESPONSE"
-			echo "skipping this file!"
-			;;
-	esac
+    	echo "v) View $file"
+    	echo "e) Edit $file"
+    	echo "c) Compile $file"
+    	echo "x) Execute $file"
+    	echo "q) Quit $file"
+    	read input
+    	case $input in
+        	v)
+            	echo "Viewing file."
+            	doThatThing "$file" "v"
+            	;;
+        	e)
+            	echo "Editing file."
+            	doThatThing "$file" "e"
+            	;;
+        	c)
+            	echo "Compiling file."
+            	doThatThing "$file" "c"
+            	;;
+        	x)
+            	echo "Executing file."
+            	doThatThing "$file" "x"
+            	;;
+        	q)
+            	echo "Quitting"
+            	exit
+            	;;
+        	*)
+            	echo "INVALID RESPONSE"
+            	echo "Skipping this file!"
+            	;;
+    	esac
 done
-doThatThing()
-{
-	local file="$1"
-	local action="$2"
-	case $action in
-		v)
-			less "$file"
-			;;
-		e)
-			nano "$file"
-			;;
-		c)
-			g++ "$file"
-			;;
-		x)
-			./"$file"
-			;;
-	esac
+
+doThatThing() {
+    local file="$1"
+    local action="$2"
+    
+    case $action in
+        v)
+            less "$file"
+            ;;
+        e)
+            nano "$file"
+            ;;
+        c)
+            g++ "$file" -o "${file%.cpp}"
+            ;;
+        x)
+            ./"$file"
+            ;;
+    esac
 }
